98ad6c9a07473e6f4af8c4ffa99bbad5dbd18ef3
baocter2004 <be93700@gmail.com>
2026-08-27 13:44:14 +0700

    add @nestjs/config and configure global settings

--- nestjs-crash-course/src/main.ts
+++ nestjs-crash-course/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 30100;
+
+  await app.listen(port);
+
+  console.log(`This app url: http://localhost:${port}`);
 }
+
 bootstrap();
